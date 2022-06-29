3c810b52e98685e87b4cdf81047376fd44dea329
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-06-29 18:53:08 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// kopiuje tablice i tworzy nową tablicę 
+
+const numbers = [1,2,3,4,5,6,7,8,9]
+
+const newNumbers = numbers 
+
+//numbers === newNumbers // true 
+
+const copyOfNumbers = numbers.slice()
+
+//numbers === copyOfNumbers // false
